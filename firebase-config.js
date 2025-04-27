96a73b17e388e5146f6f3fe1d38a074de117291e
shinwayama <shinwayama36@gmail.com>
2025-04-27 18:59:34 +0630

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Replace with your Firebase config
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    databaseURL: "YOUR_DATABASE_URL",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
